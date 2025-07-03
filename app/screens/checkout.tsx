3b46a73a137c1bda5f1862159a40b2a1d7c41274
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-07-03 21:51:18 +0400

    wip

--- app/screens/checkout.tsx
+++ app/screens/checkout.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
+
 import {
   View,
   Text,
